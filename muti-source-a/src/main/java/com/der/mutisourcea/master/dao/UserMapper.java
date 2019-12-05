ac257442a872dc58f3b77e78f5549e9f062b1248
derdrache <1297886375@qq.com>
2019-12-05 20:22:40 +0800

    mybatis-generator生成文件

--- muti-source-a/src/main/java/com/der/mutisourcea/master/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.der.mutisourcea.master.dao;
-
-import com.der.mutisourcea.master.entity.User;
-
-public interface UserMapper {
-    int insert(User record);
-
-    int insertSelective(User record);
-}
